7ad60a9a709df879c67be61d2877768333a90d20
Michał Kuźba <kmichael00968@gmail.com>
2018-03-23 14:00:07 +0100

    add first version of simpl, === and nullable

--- RegExtra.hs
+++ RegExtra.hs
@@ -10,20 +10,40 @@ class Equiv a where
   (===) :: a -> a -> Bool
 
 instance (Eq c) => Equiv (Reg c) where
-   r1 === r2 = False
+   x === y = equal (simpl x) (simpl y) where
+     equal (Lit a) (Lit b) = a == b
+     equal x y = True
 
 instance Mon (Reg c) where
-  m1 = Empty
-  x <> y = Empty
+  m1 = Eps
+  x <> y = (simpl x) :> (simpl y)
   
 simpl :: Reg c -> Reg c
+simpl (x :| y) = merger (simpl x) (simpl y) where
+  merger Empty x = x
+  merger x Empty = x
+  merger Eps Eps = Eps
+  merger x y = x :| y
+simpl (Eps :> x) = simpl x
+simpl (x :> Eps) = simpl x
+simpl (Empty :> x) = Empty
+simpl (x :> Empty) = Empty
 simpl x = x
 
 nullable :: Reg c -> Bool
-nullable x = False
+nullable x = null_helper simpler where
+  null_helper Eps = True
+  null_helper (Many _) = True
+  null_helper (x :| y) = null_helper x || null_helper y
+  null_helper (x :> y) = null_helper x && null_helper y
+  null_helper _ = False
+  simpler = simpl x
 
 empty :: Reg c -> Bool 
-empty r = False
+empty z = empty_helper simpler where
+  empty_helper Empty = True
+  empty_helper _ = False
+  simpler = simpl z
 
 der :: c -> Reg c -> Reg c
 der c r = r
